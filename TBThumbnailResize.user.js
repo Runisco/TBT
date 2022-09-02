5c7d196a681e4c1145eabaf2ce7f5c79fd9b2e78
Runisco <runisco@gmail.com>
2022-09-02 11:58:44 +0200

    Update TBThumbnailResize.user.js

--- TBThumbnailResize.user.js
+++ TBThumbnailResize.user.js
@@ -1,7 +1,10 @@
 // ==UserScript==
 // @name         TBT
 // @namespace    https://github.com/runisco
-// @version      1.0.0
+// @version      2.0.0
+// @updateURL    https://github.com/Runisco/TBThumbnailResize/raw/main/TBThumbnailResize.user.js
+// @downloadURL  https://github.com/Runisco/TBThumbnailResize/raw/main/TBThumbnailResize.user.js
+// @supportURL   https://github.com/Runisco/TBThumbnailResize/issues
 // @description  Resizes the thumbnails to make them easier to see
 // @author       Runisco
 // @match        https://simpcity.su/*
